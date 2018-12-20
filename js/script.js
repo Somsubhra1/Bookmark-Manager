e5a2e75e44c2870ce437008491b7a5190742dcb7
Somsubhra Das <das.somsubhra1@gmail.com>
2018-12-21 00:10:45 +0530

    Main script upload and minor CSS changes

--- js/script.js
+++ js/script.js
@@ -10,6 +10,10 @@ function saveBookmark(event) {
   var siteName = document.getElementById("siteName").value;
   var siteURL = document.getElementById("siteURL").value;
 
+  if (!validateFormInput(siteName, siteURL)) {
+    return false;
+  }
+
   var bookmark = {
     name: siteName,
     url: siteURL
@@ -33,6 +37,14 @@ function saveBookmark(event) {
     // set back bookmarks to localStorage
     localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
   }
+
+  // clear form
+  document.getElementById("siteName").value = "";
+  document.getElementById("siteURL").value = "";
+  window.scrollTo(0, document.body.scrollHeight);
+
+  // re-fetch bookmarks
+  fetchBookmarks();
 }
 
 // Fetching bookmarks to display
@@ -51,8 +63,47 @@ function fetchBookmarks() {
     var name = bookmarks[index].name;
     var url = bookmarks[index].url;
 
+    // URL validation
+
+    if (!url.includes("https") || !url.includes("http")) {
+      url = "http://" + url;
+    } else if (!url.includes("www")) {
+      url = "http://www." + url;
+    }
     // TODO: Design the output
     // designing output
-    bookmarksResult.innerHTML += `<div class="card card-body bg-light"><h3>${name}</h3></div>`;
+    bookmarksResult.innerHTML += `<div class="card card-body bg-light row"><div class="col-sm"><h3>${name}</h3></div><div class="col-sm"><a class="btn btn-primary visitBtn" target="__blank" href=${url}>Visit</a></div><div class="col-sm"><button class="btn btn-danger deleteBtn" onClick="deleteBookmark(\'${url}\')">Delete</button></div></div>`;
   }
+}
+
+// delete bookmarks
+function deleteBookmark(url) {
+  console.log(url);
+  var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+  //loop through bookmarks
+  for (let index = 0; index < bookmarks.length; index++) {
+    if (url.includes(bookmarks[index].url)) {
+      bookmarks.splice(index, 1);
+    }
+  }
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+
+  // re-fetch bookmarks
+  fetchBookmarks();
+}
+
+// input validation
+function validateFormInput(siteName, siteURL) {
+  if (!siteName || !siteURL) {
+    alert("Please fill the form");
+    return false;
+  }
+  var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+  var regex = new RegExp(expression);
+
+  if (!expression.test(siteURL)) {
+    alert("Enter a valid URL!");
+    return false;
+  }
+  return true;
 }
